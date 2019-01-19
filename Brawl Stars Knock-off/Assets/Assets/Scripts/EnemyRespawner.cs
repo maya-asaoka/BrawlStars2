916e1b40f29ba3023bced2f47cc54af636d67ecf
maya <mayamarina98@gmail.com>
2019-01-19 14:03:01 -0800

    added assets

---
+++ Brawl Stars Knock-off/Assets/Assets/Scripts/EnemyRespawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Respawns killed enemies at the origin after respawnTime seconds
+
+public class EnemyRespawner : MonoBehaviour {
+
+    public static EnemyRespawner instance;
+
+    public GameObject enemyPrefab;
+    public int respawnTime;
+
+    private bool respawn;
+    private float respawnEnemyCalledTime;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (respawn && Time.time - respawnEnemyCalledTime > respawnTime)
+        {
+            Instantiate(enemyPrefab, Vector3.zero, Quaternion.identity);
+            respawn = false;
+        }
+    }
+
+    // called from EnemyController after Enemy object is destroyed
+    public void RespawnEnemy()
+    {
+        respawnEnemyCalledTime = Time.time;
+        respawn = true;
+    }
+}
---
+++ Brawl Stars Knock-off/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject playerBulletPrefab;
+
+    public float speed;
+    public float turnSpeed;
+
+    private Rigidbody rb;
+
+    // raise bullets slightly up off the ground
+    private Vector3 raiseBullet = new Vector3(0, 1f, 0);
+
+	
+	void Start () {
+        rb = GetComponent<Rigidbody> ();
+	}
+
+    // player can only move straight forward and backward, horizontal input adds torque
+    private void FixedUpdate()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Instantiate(playerBulletPrefab, transform.position + raiseBullet, transform.rotation);
+        }
+
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = transform.forward * vertical * speed * Time.deltaTime;
+
+        float torque = horizontal * turnSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0f, torque, 0f);
+
+        rb.MoveRotation(rb.rotation * rotation);
+        rb.MovePosition(rb.position + movement);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            GameController.instance.CollectCoin();
+        }
+        if (other.tag == "EnemyBullet")
+        {
+            Destroy(other.gameObject);
+            GameController.instance.PlayerHit();
+        }
+    }
+}
