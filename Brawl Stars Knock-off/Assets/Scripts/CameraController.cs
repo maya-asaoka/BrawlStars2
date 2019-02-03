800ebb36790dcacd837ad6815e5358642ce6b02f
maya <mayamarina98@gmail.com>
2019-02-02 16:17:26 -0800

    added gun

---
+++ Brawl Stars Knock-off/Assets/Scripts/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -90F;
+    public float maximumX = 90F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    void Update()
+    {
+
+
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        }
+        //else if (axes == RotationAxes.MouseX)
+        //{
+        //    transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+
+        //    print("inside X");
+        //}
+        //else
+        //{
+        //    rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        //    rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        //    transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        //    print("inside Y");
+        //}
+    }
+
+    void Start()
+    {
+        //if(!networkView.isMine)
+        //enabled = false;
+
+        // Make the rigid body not change rotation
+        //if (rigidbody)
+        //rigidbody.freezeRotation = true;
+    }
+}
--- Brawl Stars Knock-off/Assets/Scripts/MovementController.cs
+++ Brawl Stars Knock-off/Assets/Scripts/MovementController.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+
+    public float speed = 0.5f;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 forward = transform.forward;
+        Vector3 move = new Vector3(x * speed * forward.x, 0, y * speed * forward.y);
+
+
+        //rb.MovePosition(rb.position + move);
+
+        // x direction
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.MovePosition(rb.position + transform.forward * speed);
+        }
+        else if (Input.GetKey(KeyCode.S)){
+            rb.MovePosition(rb.position + (-transform.forward * speed));
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 left = new Vector3(-forward.z, 0, forward.x);
+            rb.MovePosition(rb.position + left * speed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 right = new Vector3(forward.z, 0, -forward.x);
+            rb.MovePosition(rb.position + (right * speed));
+        }
+
+
     }
 }
